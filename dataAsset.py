713f319300215ef606323af63771b8ff4666c8ed
kritsada <newtronetic@gmail.com>
2023-05-16 14:40:33 +0700

    format data for use

--- dataAsset.py
+++ dataAsset.py
@@ -2,23 +2,51 @@ import pandas as pd
 import os
 from sklearn.feature_selection import SelectKBest, chi2, mutual_info_classif, RFE
 from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing
+
 
 outdir = './cleanData'
+le = preprocessing.LabelEncoder()
+
+colForConverstToInt = [
+    'ฐานความผิดหลัก',
+    'ฐานความผิดข้อหา',
+    'ครั้งที่กระทำความผิด',  # target
+    'อัตราโทษ',
+    'กลุ่มASSIST'
+]
+# data_frame.loc[:, colForConverstToInt]
 
 # clean data
 data_frame = pd.read_csv("../Data/clean/clean.csv", encoding="TIS-620")
+for col in data_frame.loc[:, colForConverstToInt]:
+    data_frame[col] = le.fit_transform(data_frame[col])
 
 # clean and balance data
 data_frame1 = pd.read_csv(
     "../Data/clean/split_data/data1.csv", encoding="TIS-620")
+for col in data_frame1.loc[:, colForConverstToInt]:
+    data_frame1[col] = le.fit_transform(data_frame1[col])
+
 data_frame2 = pd.read_csv(
     "../Data/clean/split_data/data2.csv", encoding="TIS-620")
+for col in data_frame2.loc[:, colForConverstToInt]:
+    data_frame2[col] = le.fit_transform(data_frame2[col])
+
 data_frame3 = pd.read_csv(
     "../Data/clean/split_data/data3.csv", encoding="TIS-620")
+for col in data_frame3.loc[:, colForConverstToInt]:
+    data_frame3[col] = le.fit_transform(data_frame3[col])
+
 data_frame4 = pd.read_csv(
     "../Data/clean/split_data/data4.csv", encoding="TIS-620")
+for col in data_frame4.loc[:, colForConverstToInt]:
+    data_frame4[col] = le.fit_transform(data_frame4[col])
+
 data_frame5 = pd.read_csv(
     "../Data/clean/split_data/data5.csv", encoding="TIS-620")
+for col in data_frame5.loc[:, colForConverstToInt]:
+    data_frame5[col] = le.fit_transform(data_frame5[col])
 
 #column = CMST_CASE_JUVENILE_REF, ฐานความผิดหลัก, ฐานความผิดข้อหา, ครั้งที่กระทำความผิด, อายุขณะกระทำความผิด, อัตราโทษ, กลุ่มASSIST, Risk, Need, SPECIAL_RN
 
@@ -45,6 +73,7 @@ def DataExcept2():
 def DataExcept3():
     return pd.concat([data_frame1, data_frame2, data_frame4, data_frame5])
 
+
 def DataExcept4():
     return pd.concat([data_frame1, data_frame2, data_frame3, data_frame5])
 
@@ -60,27 +89,22 @@ def createOriginalDataSet():
         '{}/{}'.format(outdir, 'original_dateset.csv'), index=False)
 
 
-def formatDataset(colConverstToInt):
+def formatDataset(colForConverstToInt):
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
     # filter only column have to use
-    colConverstToInt = [
-        'ฐานความผิดหลัก',
-        'ฐานความผิดข้อหา',
-        'ครั้งที่กระทำความผิด',  # target
-        'อัตราโทษ',
-        'กลุ่มASSIST'
-    ]
+
     # convert string to int
-    columns = data_frame.loc[:, colConverstToInt]
+    columns = data_frame.loc[:, colForConverstToInt]
 
     for col in columns:
         columns.loc[:, col], mapping = pd.factorize(columns[col])
 
     # create temp dataset for reverse after test data
     new_data_frame = data_frame.copy()
-    new_data_frame.loc[:, colConverstToInt] = columns.loc[:, colConverstToInt]
+    new_data_frame.loc[:, colForConverstToInt] = columns.loc[:,
+                                                             colForConverstToInt]
 
     # create file for new data
 
