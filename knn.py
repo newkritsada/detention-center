2afacaacf2d1ac3a93b50e783c45c093e0745295
kritsada <newtronetic@gmail.com>
2023-05-17 12:53:44 +0700

    get time for test

--- knn.py
+++ knn.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
 # from joblib import dump
 
 import dataAsset
@@ -11,8 +9,16 @@ import dataAsset
 def knn_predict(x_train, x_test, y_train, y_test):
     y_train = np.ravel(y_train)
     knn = KNeighborsClassifier(n_neighbors=3)
+
+    start_train_time = time.time()
     knn.fit(x_train, y_train)
+    end_train_time = time.time()
+    training_time = end_train_time - start_train_time
+
+    start_test_time = time.time()
     predictions = knn.predict(x_test)
+    end_test_time = time.time()
+    testing_time = end_test_time - start_test_time
 
     accuracy_train = knn.score(x_train, y_train)*100
     accuracy_test = knn.score(x_test, y_test)*100
@@ -24,6 +30,9 @@ def knn_predict(x_train, x_test, y_train, y_test):
     print('The accuracy on test data is {:.2f}%'.format(
         accuracy_test))
     print('Predictions: {}, {} data'.format(predict, predict_len))
+
+    print("Training time:", training_time)
+    print("Testing time:", testing_time)
 
     return accuracy_train, accuracy_test, predict, predict_len
 
