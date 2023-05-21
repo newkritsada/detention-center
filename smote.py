5a527ee5201e2fc6495511ee533d02f50ab5d070
kritsada <newtronetic@gmail.com>
2023-05-21 18:40:58 +0700

    use text from data_asset

--- smote.py
+++ smote.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from imblearn.over_sampling import SMOTE
 
+from data_asset import outdir, original_file_name
+
 # อ่านข้อมูลจากไฟล์ CSV
-data = pd.read_csv('original_dataset.csv')
+data = pd.read_csv(outdir+'/'+original_file_name)
 
 # แยกข้อมูลเชิงปริมาณและเป้าหมาย
 X = data.drop(['repeated'], axis=1)
@@ -15,7 +17,8 @@ smote = SMOTE()
 X_resampled, y_resampled = smote.fit_resample(X, y)
 
 # สร้าง DataFrame ของข้อมูลที่ขยายแล้ว
-resampled_data = pd.concat([pd.DataFrame(X_resampled), pd.Series(y_resampled, name='ครั้งที่กระทำความผิด')], axis=1)
+resampled_data = pd.concat([pd.DataFrame(X_resampled), pd.Series(
+    y_resampled, name='ครั้งที่กระทำความผิด')], axis=1)
 
 # บันทึกข้อมูลที่ขยายแล้วเป็นไฟล์ CSV
 resampled_data.to_csv('smote_dataset.csv', index=False)
