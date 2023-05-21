382feedd10e245a3bd6568ae7d2cb6f545975c41
kritsada <newtronetic@gmail.com>
2023-05-21 18:57:25 +0700

    lean code

--- knn_with_chi.py
+++ knn_with_chi.py
@@ -1,25 +1,20 @@
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
 from sklearn.metrics import classification_report, confusion_matrix
 
-import data_asset
+from data_asset import data_frame, Chi
 from model import knn_predict
 
 
 print('\n\n===== k-Nearest Neighbors =====\n\n')
 
-data = data_asset.data_frame
-
-le = preprocessing.LabelEncoder()
-for col in data.loc[1:]:
-    data[col] = le.fit_transform(data[col])
+data = data_frame
 
 XX = data.drop(['CMST_CASE_JUVENILE_REF', 'ครั้งที่กระทำความผิด'], axis=1)
 yy = data['ครั้งที่กระทำความผิด']
 # XX = (XX - XX.mean()) / XX.std()
 
 # Perform feature selection using chi-square test
-X_new = data_asset.Chi(XX, yy)
+X_new = Chi(XX, yy)
 
 x_train, x_test, y_train, y_test = train_test_split(
     X_new, yy, test_size=0.3, random_state=45)
